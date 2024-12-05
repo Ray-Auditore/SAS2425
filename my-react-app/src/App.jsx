335c3ed2b8cc198fcbd46dd43655c8ea3c2411b3
Life-In-Rio <151702591+Ray-Auditore@users.noreply.github.com>
2024-12-05 09:13:10 +0700

    Praktek SAS 2021

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function Card() {
+  return (
+    <header>
+      <h1>Judul</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium,
+        accusanam!
+      </p>
+     <img src="../public/image/doa.jpg" alt="" />
+    </header>
+  );
+}
+
+export default Card;
